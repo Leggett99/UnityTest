6bb29703dc7f841b7429d4ed0e867dfa7eba0c69
Leggett99 <joshualeggett99@outlook.com>
2016-08-04 14:22:42 +1000

    Additions
    Added payCoke and added a majority of variables that I need

---
+++ Vending Machine/src/payCoke.java
@@ -0,0 +1,19 @@
+
+public class payCoke extends Main{
+	public static void process(){
+		if(stockCoke < 1){
+			if(wallet >= 4.45){
+				
+			}
+			else{
+				
+			}
+		}
+		//no stock
+		else{
+			
+		}
+	}
+	
+
+}
